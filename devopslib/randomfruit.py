57aaaa56ac5e5e117f459da2d0cf2b9509d66317
Vikas Pal <93254870+vikas807@users.noreply.github.com>
2023-05-26 22:31:35 +0000

    added command line tool

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -4,3 +4,13 @@ from random import choices
 def fruit():
     fruits = ["apple", "mango", "banana"]
     return choices(fruits)[0]
+
+
+def meal(beverage):
+    my_fruit = fruit()
+    print(f"Your fruit is {my_fruit} and your beverage is {beverage}")
+    if my_fruit == "cherry":
+        complete_meal = f"your meal is {my_fruit} and {beverage}"  
+        return complete_meal
+    alternate_meal = f"your meal is steak and {beverage}"
+    return alternate_meal
